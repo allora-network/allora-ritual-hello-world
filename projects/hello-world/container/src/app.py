6992baee2272bac7a4341df917325f116ec67fe2
Derek Anderson <derek@upshot.xyz>
2024-02-20 19:10:15 -0600

    base infernet allora
    Signed-off-by: Derek Anderson <derek@upshot.xyz>

--- projects/hello-world/container/src/app.py
+++ projects/hello-world/container/src/app.py
@@ -1,5 +1,5 @@
 from typing import Any
-
+import requests
 from flask import Flask, request
 
 
@@ -13,6 +13,27 @@ def create_app() -> Flask:
     @app.route("/service_output", methods=["POST"])
     def inference() -> dict[str, Any]:
         input = request.json
-        return {"output": f"hello, world!, your input was: {input}"}
+        url = 'http://host.docker.internal:8081/api/v1/functions/execute'
+        payload = {
+            "function_id": "bafybeiaugwh3mktzbnudurzk7jcyvmdhyy6jnairiu2phuf2t7c7orowjq",
+            "method": "footest.wasm",
+            "parameters": None,
+            "config": {
+                "env_vars": [
+                    {"name": "BLS_REQUEST_PATH", "value": "/api"},
+                ],
+                "number_of_nodes": -1,
+                "timeout": 2
+            }
+        }
+
+
+        # Set the Content-Type header
+        headers = {'Content-Type': 'application/json'}
+
+        # Make the POST request
+        response = requests.post(url, json=payload, headers=headers)
+
+        return {"output": f"hello, world!, your input was: {input}", "response" : response.text}
 
     return app
